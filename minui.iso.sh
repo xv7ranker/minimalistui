09bc3711a25bb0eafebac9f615dcd6a65a7de575
xv7ranker <87756964+xv7ranker@users.noreply.github.com>
2025-12-16 22:01:27 +0700

    .sh script to automate minui.iso creation (Fix2)

--- minui.iso.sh
+++ minui.iso.sh
@@ -8,7 +8,6 @@ L="/mnt/sfs0"
 S="$Z/arch/x86_64/airootfs.sfs"
 G="$L/minui"
 C="$L/minimalistui.sh"
-DV=""
 M=$(find . -maxdepth 1 -type f -name "*.iso" | head -n 1)
 while true; do
 if [[ $EUID -ne 0 ]]; then #1st stage, sudo
@@ -48,69 +47,64 @@ is_fs_larger_than_gib() {
     echo "Free disk space is >5GB, continuing"
     read -r -p "What would you want your bootable .iso version be?: " V #2nd stage, read
     read -r -p "What would you want your bootable .iso output file name be? (don't add file extension): " I
-    read -r -p "Would you like to use fancy mode (no command looks, 1) or default (running commands are visible, 0)? " FC
-    case $FC in
-    1) DV="> /dev/null" ;;
-    *) ;;
-    esac
     echo "Initiating minui.iso creation process." #3rd stage, start
     echo "Mounting arch.iso"
     echo "Mounting $M as arch.iso in directory /mnt/iso"
-    mkdir -p $X $DV
-    mkdir -p $Z $DV
-    mkdir -p $L $DV
-    mount $M $X $DV
-    cp -a $X/. $Z $DV
-    cd $Z $DV
-    unsquashfs -f -d $L $S $DV
+    mkdir -p $X
+    mkdir -p $Z
+    mkdir -p $L
+    mount $M $X
+    cp -a $X/. $Z
+    cd $Z
+    unsquashfs -f -d $L $S
     RC=""
     RM="
-    rm -rf $L/mkisosfs.sh $DV 
-    rm -rf $L/minui.iso.sh $DV"
+    rm -rf $L/mkisosfs.sh
+    rm -rf $L/minui.iso.sh"
     read -r -p "Would you like to create 'Creator Edition' (-ce) .iso? (1 for yes, 0 for no (default: no))" Y
     case $Y in
     1) IM="$I-ce" 
         RM="" 
         RC="
-        mv $L/mkisosfs.sh $DV
-        mv $L/minui.iso.sh $DV" ;;
+        mv $L/mkisosfs.sh
+        mv $L/minui.iso.sh" ;;
     *) IM="$I" ;;
     esac
     if [[ -d "$A/minui" && -f "$A/minimalistui.sh" ]]; then
-      mv minui $G $DV
-      mv minimalistui.sh $C $DV
+      mv minui $G
+      mv minimalistui.sh $C
       $RC
     else
       echo "'minui' and minimalistui.sh is not spotted, using git clone to get both files"
-      git clone https://github.com/xv7ranker/minimalistui-extras $G $DV
-      git clone https://github.com/xv7ranker/minimalistui $L $DV
-      rm -rf $L/.git $DV
-      rm -rf $L/README.md $DV
-      rm -rf $L/LICENSE $DV
+      git clone https://github.com/xv7ranker/minimalistui-extras $G
+      git clone https://github.com/xv7ranker/minimalistui $L
+      rm -rf $L/.git
+      rm -rf $L/README.md
+      rm -rf $L/LICENSE
       $RM
     fi
-    chmod +x $L/minui/x.sh $DV
-    chmod +x $L/minui/c.sh $DV
-    chmod +x $C $DV
-    mv $C $L/usr/bin/minimalistui.sh $DV
+    chmod +x $L/minui/x.sh
+    chmod +x $L/minui/c.sh
+    chmod +x $C
+    mv $C $L/usr/bin/minimalistui.sh
     $RC
-    cd $L $DV
+    cd $L
     echo "creating .sfs" #4th stage, .sfs $DV
-    mksquashfs "." "airootfs.sfs" -comp gzip -b 1M -no-progress $DV
-    echo ".sfs creation finished" $DV
-    J=$(find . -maxdepth 1 -type f -name "*.sfs" | head -n 1) $DV
-    rm -rf $S $DV
-    mv $J $S $DV
-    cd $Z $DV
+    mksquashfs "." "airootfs.sfs" -comp gzip -b 1M -no-progress
+    echo ".sfs creation finished"
+    J=$(find . -maxdepth 1 -type f -name "*.sfs" | head -n 1)
+    rm -rf $S
+    mv $J $S
+    cd $Z
     echo "creating .iso" # 5th stage, .iso
-    xorriso -as mkisofs -D -r -J -l -V "$V" -o "${I}.iso" -p "xv7ranker" -publisher "xv7ranker" -no-emul-boot -boot-load-size 4 -boot-info-table -b boot/syslinux/isolinux.bin -c boot/syslinux/boot.cat -eltorito-alt-boot -e EFI/BOOT/BOOTx64.EFI -no-emul-boot -isohybrid-mbr  boot/syslinux/isohdpfx.bin "." $DV
+    xorriso -as mkisofs -D -r -J -l -V "$V" -o "${I}.iso" -p "xv7ranker" -publisher "xv7ranker" -no-emul-boot -boot-load-size 4 -boot-info-table -b boot/syslinux/isolinux.bin -c boot/syslinux/boot.cat -eltorito-alt-boot -e EFI/BOOT/BOOTx64.EFI -no-emul-boot -isohybrid-mbr  boot/syslinux/isohdpfx.bin "."
     echo ".iso creation finished"
-    F=$(find . -maxdepth 1 -type f -name "*.iso" | head -n 1) $DV
-    K=$(basename "$F") $DV
-    cp $K "$A"/$K $DV
-    umount $X $DV
-    rm -rf $X $DV
-    rm -rf $Z $DV
-    rm -rf $L $DV
+    F=$(find . -maxdepth 1 -type f -name "*.iso" | head -n 1)
+    K=$(basename "$F")
+    cp $K "$A"/$K
+    umount $X
+    rm -rf $X
+    rm -rf $Z
+    rm -rf $L
     echo "Script finished. Exiting"
     exit 0
