937d75de262b9c2b066ecdd6fd878c9ce9855eff
xv7ranker <87756964+xv7ranker@users.noreply.github.com>
2026-01-02 15:24:54 +0700

    Delete minui/l.sh

--- minui/l.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-#  MinimalistUI Labwc Autostart
-#  Location: ~/.config/labwc/autostart
-#
-swaybg -i /usr/minui/backgrounds/wp.png -m fill &
-waybar &
-mako &
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
